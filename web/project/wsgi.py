a7f9d04bde0df7a38b61ed60dde91af51265b4d7
elghibaomar <elghibaomar@gmail.com>
2022-07-17 18:59:41 +0100

    tst f5

--- web/project/wsgi.py
+++ web/project/wsgi.py
@@ -17,7 +17,7 @@ from django.core.wsgi import get_wsgi_application
 
 CURRENT_DIR = pathlib.Path(__file__).resolve().parent
 BASE_DIR = CURRENT_DIR.parent
-ENV_FILE_PATH = BASE_DIR / ".env"
+ENV_FILE_PATH = BASE_DIR / ".env.prod"
 
 dotenv.read_dotenv(str(ENV_FILE_PATH))
 
