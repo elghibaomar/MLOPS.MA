9a567980608008d09de61280069074f85bd6ca46
elghibaomar <elghibaomar@gmail.com>
2022-07-20 17:18:18 +0100

    l1

--- web/project/settings.py
+++ web/project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = str(os.environ.get("DEBUG")) == "1"
-DEBUG = False
+DEBUG = True
 
 # ENV_ALLOWED_HOST = os.environ.get("ENV_ALLOWE_HOST")
 
